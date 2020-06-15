df37fa078883b4f7aaea4173244d63ca554edcd1
nanpangyou <yigehd@foxmail.com>
2020-06-15 11:15:05 +0800

    增加button的单元测试，使用chai.js,点击事件的mock使用chai-spies

--- src/app.js
+++ src/app.js
@@ -9,8 +9,100 @@ Vue.component("m-icon", Micon);
 let app = new Vue({
   el: "#app",
   data: {
-    loading1: true,
-    loading2: true,
-    loading3: true
+    loading1: false,
+    loading2: false,
+    loading3: false
   }
 });
+
+import chai, { expect } from "chai";
+import spies from "chai-spies";
+chai.use(spies);
+{
+  // 测试button是否含有name属性
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+  const Constructor = Vue.extend(Mbutton);
+  const vm = new Constructor({
+    propsData: {
+      name: "setting"
+    }
+  });
+  vm.$mount(div);
+  const iconName = vm.$el.querySelector("use").getAttribute("xlink:href");
+  expect(iconName).is.eq("#i-setting");
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  //测试button是否可以传loading
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+  const Constructor = Vue.extend(Mbutton);
+  const vm = new Constructor({
+    propsData: {
+      name: "setting",
+      loading: true
+    }
+  });
+  vm.$mount(div);
+  const icons = vm.$el.querySelectorAll("use");
+  // 当传入loading和其他icon时，只有loading生效
+  expect(icons.length).eq(1);
+  const iconName = icons[0].getAttribute("xlink:href");
+  expect(iconName).is.eq("#i-loading");
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  // 测试iconPositon属性(icon在左侧)
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+  const Constructor = Vue.extend(Mbutton);
+  const vm = new Constructor({
+    propsData: {
+      name: "setting",
+      loading: true
+    }
+  });
+  vm.$mount(div);
+  const { order } = getComputedStyle(vm.$el.querySelector("svg"));
+  expect(order).eq("1");
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  // 测试iconPositon属性(icon在右侧)
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+  const Constructor = Vue.extend(Mbutton);
+  const vm = new Constructor({
+    propsData: {
+      name: "setting",
+      loading: true,
+      iconPosition: "right"
+    }
+  });
+  vm.$mount(div);
+  const { order } = getComputedStyle(vm.$el.querySelector("svg"));
+  expect(order).eq("2");
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  // 测试按钮点击
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+  const Constructor = Vue.extend(Mbutton);
+  const vm = new Constructor({
+    propsData: {
+      name: "setting",
+      loading: true,
+      iconPosition: "right"
+    }
+  }).$mount(div);  //测试点击的时候必须要挂载到页面上
+  const spy = chai.spy(() => {});
+  vm.$on("click", spy);
+  vm.$el.click();
+  expect(spy).have.been.called();
+}
