0399c49060e4582240a2d87b6cced43f862909e2
nanpangyou <yigehd@foxmail.com>
2020-06-30 17:30:19 +0800

    更新iconfont文件，添加多种响应式尺寸，向小尺寸兼容

--- src/component/grid/col.vue
+++ src/component/grid/col.vue
@@ -6,6 +6,11 @@
   </div>
 </template>
 <script>
+const validator = (value) => {
+  return Object.keys(value)
+    .map((i) => ["span", "offset"].includes(i))
+    .every((i) => i === true);
+};
 export default {
   props: {
     span: {
@@ -14,14 +19,12 @@ export default {
     offset: {
       type: [Number, String]
     },
-    xs: {
-      type: Object,
-      validator(value) {
-        return Object.keys(value)
-          .map((i) => ["span", "offset"].includes(i))
-          .every((i) => i === true);
-      }
-    }
+    xs: { type: Object, validator },
+    sm: { type: Object, validator },
+    md: { type: Object, validator },
+    lg: { type: Object, validator },
+    xl: { type: Object, validator },
+    xxl: { type: Object, validator }
   },
   data() {
     return {
@@ -44,11 +47,16 @@ export default {
       return this.gutter ? `padding: 0 ${halfGutter}px` : "";
     },
     colClass() {
-      const { span, offset, xs } = this;
+      const { span, offset, xs, sm, md, lg, xl, xxl } = this;
       return [
         span && `m-col-${span}`,
         offset && `m-col-offset-${offset}`,
-        ...this.getClass(xs, "xs")
+        ...this.getClass(xs, "xs"),
+        ...this.getClass(sm, "sm"),
+        ...this.getClass(md, "md"),
+        ...this.getClass(lg, "lg"),
+        ...this.getClass(xl, "xl"),
+        ...this.getClass(xxl, "xxl")
       ];
     }
   }
@@ -70,6 +78,56 @@ export default {
         width: ($i/24) * 100%;
       }
       &.m-xs-col-offset-#{$i} {
+        margin-left: ($i/24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 576px) {
+    @for $i from 1 through 24 {
+      &.m-sm-col-#{$i} {
+        width: ($i/24) * 100%;
+      }
+      &.m-sm-col-offset-#{$i} {
+        margin-left: ($i/24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 768px) {
+    @for $i from 1 through 24 {
+      &.m-md-col-#{$i} {
+        width: ($i/24) * 100%;
+      }
+      &.m-md-col-offset-#{$i} {
+        margin-left: ($i/24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 992px) {
+    @for $i from 1 through 24 {
+      &.m-lg-col-#{$i} {
+        width: ($i/24) * 100%;
+      }
+      &.m-lg-col-offset-#{$i} {
+        margin-left: ($i/24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 1200px) {
+    @for $i from 1 through 24 {
+      &.m-xl-col-#{$i} {
+        width: ($i/24) * 100%;
+      }
+      &.m-xl-col-offset-#{$i} {
+        margin-left: ($i/24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 1600px) {
+    @for $i from 1 through 24 {
+      &.m-xxl-col-#{$i} {
+        width: ($i/24) * 100%;
+      }
+      &.m-xxl-col-offset-#{$i} {
         margin-left: ($i/24) * 100%;
       }
     }
