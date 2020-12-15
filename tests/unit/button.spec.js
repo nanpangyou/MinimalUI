fbd3a23a96db25962e23cb71cc8228b3dba8837b
nanpangyou <yigehd@foxmail.com>
2020-12-15 17:49:20 +0800

    mod: 升级部分测试，使用Vue Test Utils测试

---
+++ tests/unit/button.spec.js
@@ -0,0 +1,64 @@
+import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import { mount } from '@vue/test-utils'
+import sinonChai from 'sinon-chai'
+import Button from "@/components/button/button";
+chai.use(sinonChai)
+
+describe('Button.vue', () => {
+  it("存在.", () => {
+    expect(Button).to.be.ok;
+  });
+  it("可以设置icon.", () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        name: "setting"
+      }
+    })
+    const useElement = wrapper.find("use")
+    expect(useElement.attributes()['href']).to.equal("#i-setting");
+  });
+  it("可以设置loading.", () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        name: "setting",
+        loading: true
+      }
+    })
+    const useElement = wrapper.find("use");
+    expect(useElement.attributes()['href']).to.equal("#i-loading");
+  });
+  xit("icon 默认的 order 是 1", () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        name: "setting"
+      }
+    })
+    const icon = wrapper.find("svg");
+    console.log(icon.html())
+    expect(getComputedStyle(icon).order).to.eq("1");
+  });
+  xit("设置 iconPosition 可以改变 order", () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        name: "setting",
+        iconPosition: "right"
+      }
+    })
+    const icon = vm.$el.querySelector("svg");
+    expect(getComputedStyle(icon).order).to.eq("2");
+    vm.$el.remove();
+    vm.$destroy();
+  });
+  it("点击 button 触发 click 事件", () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        name: "setting"
+      }
+    })
+    const callback = sinon.fake();
+    wrapper.vm.$on('click', callback)
+    wrapper.trigger('click');
+    expect(callback).to.have.been.called;
+  });
+})
---
+++ tests/unit/input.spec.js
@@ -0,0 +1,93 @@
+import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import { mount } from '@vue/test-utils'
+import sinonChai from 'sinon-chai'
+import Input from "@/components/input/input";
+chai.use(sinonChai)
+
+
+
+describe("Input.vue", () => {
+    it("存在.", () => {
+        expect(Input).to.be.ok;
+    });
+    describe("props 属性部分", () => {
+        let wrapper = null;
+        beforeEach(() => {
+            // Constructor = Vue.extend(Input);
+        });
+        afterEach(() => {
+            // vm.$el.remove();
+            // vm.$destroy();
+        });
+        it("可以设置value.", () => {
+            wrapper = mount(Input, {
+                propsData: {
+                    value: "xxx"
+                }
+            });
+            const inputElement = wrapper.find("input");
+            expect(inputElement.element.value).to.equal("xxx");
+        });
+        it("可以设置disabled.", () => {
+            wrapper = mount(Input, {
+                propsData: {
+                    disabled: true
+                }
+            });
+            const inputElement = wrapper.find('input');
+            expect(inputElement.attributes()['disabled']).to.equal("disabled");
+        });
+        it("可以设置readonly", () => {
+            wrapper = mount(Input, {
+                propsData: {
+                    readonly: true
+                }
+            });
+            const inputElement = wrapper.find("input");
+            expect(inputElement.attributes()["readonly"]).to.equal("readonly");
+        });
+        it("设置 error ", () => {
+            wrapper = mount(Input, {
+                propsData: {
+                    error: "you are wrong"
+                }
+            });
+            const errorIconElement = wrapper.find(".icon");
+            expect(
+                errorIconElement.find('use').attributes()['href']
+            ).to.equal("#i-error");
+            const errorMsgElement = wrapper.find("span");
+            expect(errorMsgElement.text()).to.equal("you are wrong");
+        });
+    });
+    describe("事件部分", () => {
+        let wrapper = null;
+        const triggerWithValueEventList = ["input"];
+        const triggerWithoutValueEventList = ["change", "focus", "blur"];
+        beforeEach(() => {
+            wrapper = mount(Input)
+        });
+        afterEach(() => {
+            // vm.$destroy();
+        });
+        triggerWithValueEventList.map((item) => {
+            it(`触发${item}事件`, () => {
+                const callback = sinon.fake();
+                wrapper.vm.$on(item, callback);
+                wrapper.vm.$emit(item)
+                wrapper.vm.$emit(item, 'hi')
+                expect(callback).to.have.been.called;
+                expect(callback).to.have.been.calledWith("hi");
+            });
+        });
+        triggerWithoutValueEventList.map((item) => {
+            it(`触发${item}事件`, () => {
+                let callback = sinon.fake();
+                wrapper.vm.$on(item, callback);
+                wrapper.vm.$emit(item);
+                expect(callback).to.have.been.called;
+            });
+        });
+    });
+});
