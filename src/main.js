89fcaad24eafd8133bd5a1b31653e86f1a931872
nanpangyou <yigehd@foxmail.com>
2021-10-17 21:41:42 +0800

    start again

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 import "./css/main.scss";
-
 import ToastPlugin from "../src/components/toast/toastPlugin";
 
 Vue.use(ToastPlugin);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount("#app");
